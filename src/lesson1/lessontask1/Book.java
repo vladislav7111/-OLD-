a08589222cb4aa4c0dddeee37b0f9e2d55924670
Vlad Salata <vladislav7111@gmail.com>
2017-10-06 21:08:47 +0300

    Initial commit

---
+++ src/lesson1/lessontask1/Book.java
@@ -0,0 +1,7 @@
+package lesson1.lessontask1;
+
+public abstract class Book {
+    void readTheBook() {
+
+    }
+}
---
+++ src/lesson1/lessontask1/BuyTheBook.java
@@ -0,0 +1,5 @@
+package lesson1.lessontask1;
+
+public interface BuyTheBook {
+    void buyTheBook();
+}
