e69e344298b4b259298a00e6618541c7fa8e672d
Vlad Salata <vladislav7111@gmail.com>
2017-10-10 21:12:47 +0300

    Default commit.

---
+++ src/lesson1/hometask1/MenuOptions.java
@@ -0,0 +1,19 @@
+package lesson1.hometask1;
+
+public class MenuOptions {
+    static void Sout1levelMenu() {
+        System.out.println("\n" +
+                "Выберите действие?\n" +
+                "1 - Операции\n" +
+                "2 - Печать\n" +
+                "3 - Выход");
+    }
+
+    static void SoutOperations() {
+        System.out.println("\n" +
+                "Выберите действие?\n" +
+                "1 - Вычисление значений\n" +
+                "2 - Поиск\n" +
+                "3 - Назад");
+    }
+}
