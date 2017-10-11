263ee1019e58be4f7eb35e63c5040c9accf0c237
Vlad Salata <vladislav7111@gmail.com>
2017-10-11 20:49:45 +0300

    Default commit.

--- src/lesson1/hometask1/Main.java
+++ src/lesson1/hometask1/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
 
+
         lesson1.hometask1.Massive.createMassive();
 
         lesson1.hometask1.Menu.createMenu();
