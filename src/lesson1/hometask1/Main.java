c784538a6ccecdd5dcb8b24c3a18d4b1a8941a03
Vlad Salata <vladislav7111@gmail.com>
2017-10-20 21:24:25 +0300

    Corrected all errors in methods.Add some needed classes.Fix bug, like uncorrect sort and display menu.

--- src/lesson1/hometask1/Main.java
+++ src/lesson1/hometask1/Main.java
@@ -4,13 +4,10 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         MassiveGenerator m = new MassiveGenerator();
         m.createMassive();
-
         Menu menu = new Menu();
         menu.createMenu();
-
     }
 }
 
